e02051e531cc69cfe56ef7a72d1e4f63406d4206
Ayanamy <annafelix448@gmail.com>
2025-01-28 16:31:44 -0300

    Merge branch 'main' of https://github.com/rannyzita/FichaBiblioteca into main

--- fichaBiblioteca/src/ui/ProdutoController.java
+++ fichaBiblioteca/src/ui/ProdutoController.java
@@ -1,10 +1,23 @@
 package ui;
 
+import java.awt.Image;
+import java.io.File;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import model.Categoria;
+import model.Produto;
+
 
 public class ProdutoController implements Initializable {
 
@@ -14,17 +27,67 @@ public class ProdutoController implements Initializable {
 	@FXML private Button btnUpload;
 	@FXML private Button btnSalvar;
 	@FXML private Button btnLimpar;
-	
-	@FXML 
-	private void handleSalvar() {
-        // Lógica de salvar produto
-    }
+	@FXML private ImageView imgProduto;
+	private byte[] imagemSelecionada;
+
 	
 	@FXML
-	private void handleUpload() {
-        // Lógica para selecionar imagem
+    private void handleSave() {
+        String nome = txtNome.getText();
+        String precoTexto = txtPreco.getText();
+        String categoriaTexto  = txtCategoria.getText();
+
+        if (nome.isEmpty() || precoTexto.isEmpty() || categoriaTexto.isEmpty() || imagemSelecionada == null) {
+            mostrarAlerta("Preencha todos os campos!", Alert.AlertType.WARNING);
+            return;
+        }
+
+        try {
+            double preco = Double.parseDouble(precoTexto);
+            Categoria categoria = converterStringParaCategoria(categoriaTexto);
+            Produto produto = new Produto(0, nome, preco, imagemSelecionada, categoria);
+            produto.setNome(nome);
+            produto.setPreco(preco);
+            produto.setCategoria(categoria);
+            produto.setImagem(imagemSelecionada);
+
+            ProdutoDAO produtoDAO = new ProdutoDAO();
+            produtoDAO.salvar(produto);
+
+            mostrarAlerta("Produto salvo com sucesso!", Alert.AlertType.INFORMATION);
+            handleClean();
+
+        } catch (NumberFormatException e) {
+            mostrarAlerta("Preço inválido!", Alert.AlertType.ERROR);
+        }
     }
 	
+	private Categoria converterStringParaCategoria(String categoriaTexto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@FXML
+    private void handleUpload() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("Imagens", "*.png", "*.jpg", "*.jpeg")
+        );
+        File file = fileChooser.showOpenDialog(null);
+
+        if (file != null) {
+            try {
+                Path path = Paths.get(file.getAbsolutePath());
+                imagemSelecionada = Files.readAllBytes(path);
+                Image imagem = new Image(file.toURI().toString());
+                imgProduto.setImage(imagem);
+            } catch (Exception e) {
+                e.printStackTrace();
+                mostrarAlerta("Erro ao carregar imagem", Alert.AlertType.ERROR);
+            }
+        }
+    }
+
 	@FXML
 	private void handleClean() {
 		txtNome.clear();
